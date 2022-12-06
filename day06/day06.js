92acf56dcb1d09304459ad80ad38451959dee40e
Rob Sassack <rsassack25@gmail.com>
2022-12-06 11:38:07 -0500

    Changed day 6 solution to read whole input instead of streaming

--- day06/day06.js
+++ day06/day06.js
@@ -1,22 +1,28 @@
 const fs = require("fs");
-let file = fs.createReadStream("./input.txt", {
-  encoding: "utf8",
-  highWaterMark: 1,
-});
-let chars = "";
-// buffer size can be changed. setting it to 4 produces the answer for part 1, 14 for part 2.
-let buffer = 14;
-let index = buffer;
-file.on("data", (input) => {
-  chars += input;
-  if (chars.length < buffer) return;
-  if (chars.length > buffer) {
-    chars = chars.slice(1);
+// read input file
+let input = fs.readFileSync("./input.txt", "utf8");
+
+function scan(buffer) {
+  let index = buffer;
+  let chars = "";
+  // loop through input file moving up to view every 4 chars
+  for (let i = 0; i < input.length; i++) {
+    chars += input[i];
+    // fill string if not at the size of the buffer yet
+    if (chars.length < buffer) continue;
+    if (chars.length > buffer) {
+      chars = chars.slice(1);
+    }
+    if (/(.).*\1/.test(chars)) {
+      index++;
+    } else {
+      return index;
+    }
   }
-  if (/(.).*\1/.test(chars)) {
-    index++;
-  } else {
-    console.log("Start-of-packet marker detected at: " + index);
-    file.close();
-  }
-});
+}
+
+// Part 1
+console.log("Start-of-packet marker (buffer 4): " + scan(4));
+
+// Part 2
+console.log("Start-of-packet marker (buffer 14): " + scan(14));
