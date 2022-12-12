a90f5fcc40a30bea33713826a491087b3393a452
Rob Sassack <rsassack25@gmail.com>
2022-12-12 01:00:01 -0500

    Day 9 - Part 2 completed

--- day09/day09.js
+++ day09/day09.js
@@ -7,17 +7,15 @@ function calcDistance(x, y) {
   return Math.sqrt(Math.pow(x[0] - y[0], 2) + Math.pow(x[1] - y[1], 2));
 }
 
-let headVisited = [[0, 0]];
-let tailVisited = [[0, 0]];
 let head = [0, 0];
-let tail = [0, 0];
+let tails = [[0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0], [0, 0]];
+let tailsVisited = [[[0, 0]], [[0, 0]], [[0, 0]], [[0, 0]], [[0, 0]], [[0, 0]], [[0, 0]], [[0, 0]], [[0, 0]]];
 input.forEach((line) => {
   let motion = line.split(' ');
   let direction = motion[0];
   let distance = parseInt(motion[1]);
   for (let i=0; i<distance; i++) {
     // move head based on instruction given
-    headVisited.push([head[0], head[1]]);
     switch (direction) {
       case 'U':
         head[1]++;
@@ -32,47 +30,63 @@ input.forEach((line) => {
         head[0]++;
         break;
     }
-    let headToTail = calcDistance(head, tail);
-    if (headToTail >= 2) {
-      // if head above tail
-      if (head[1] > tail[1] && head[0] === tail[0]) {
-        tail[1]++;
-      }
-      // if head below tail
-      else if (head[1] < tail[1] && head[0] === tail[0]) {
-        tail[1]--;
-      }
-      // if head to the right of tail
-      else if (head[0] > tail[0] && head[1] === tail[1]) {
-        tail[0]++;
-      }
-      // if head to the left of tail
-      else if (head[0] < tail[0] && head[1] === tail[1]) {
-        tail[0]--;
+    for (let i=0; i<tails.length; i++) {
+      if (i === 0) {
+        moveNode(head, tails[i]);
+        tailsVisited[i].push([tails[i][0], tails[i][1]]);
       } else {
-        // if head is diagonal to tail
-        if (head[0] > tail[0] && head[1] > tail[1]) {
-          tail[0]++;
-          tail[1]++;
-        } else if (head[0] > tail[0] && head[1] < tail[1]) {
-          tail[0]++;
-          tail[1]--;
-        } else if (head[0] < tail[0] && head[1] > tail[1]) {
-          tail[0]--;
-          tail[1]++;
-        } else if (head[0] < tail[0] && head[1] < tail[1]) {
-          tail[0]--;
-          tail[1]--;
-        }
+        moveNode(tails[i-1], tails[i]);
+        tailsVisited[i].push([tails[i][0], tails[i][1]]);
       }
-      tailVisited.push([tail[0], tail[1]]);
     }
   }
 })
+
+function moveNode(leader, follower) {
+  let headToTail = calcDistance(leader, follower);
+  if (headToTail >= 2) {
+    // if head above tail
+    if (leader[1] > follower[1] && leader[0] === follower[0]) {
+      follower[1]++;
+    }
+
+    // if head below tail
+    else if (leader[1] < follower[1] && leader[0] === follower[0]) {
+      follower[1]--;
+    }
+
+    // if head to the right of tail
+    else if (leader[0] > follower[0] && leader[1] === follower[1]) {
+      follower[0]++;
+    }
+
+    // if head to the left of tail
+    else if (leader[0] < follower[0] && leader[1] === follower[1]) {
+      follower[0]--;
+    } else {
+      // if head is diagonal to tail
+      if (leader[0] > follower[0] && leader[1] > follower[1]) {
+        follower[0]++;
+        follower[1]++;
+      } else if (leader[0] > follower[0] && leader[1] < follower[1]) {
+        follower[0]++;
+        follower[1]--;
+      } else if (leader[0] < follower[0] && leader[1] > follower[1]) {
+        follower[0]--;
+        follower[1]++;
+      } else if (leader[0] < follower[0] && leader[1] < follower[1]) {
+        follower[0]--;
+        follower[1]--;
+      }
+    }
+  }
+}
 
 // Part 1
 function removeDuplicates(array) {
   return Array.from(new Set(array.map(JSON.stringify)), JSON.parse);
 }
+console.log("Rope tail visited spots: " + removeDuplicates(tailsVisited[0]).length);
 
-console.log("Rope tail visited spots: " + removeDuplicates(tailVisited).length);
+// Part 2
+console.log("9th knot visited spots: " + removeDuplicates(tailsVisited[8]).length);
