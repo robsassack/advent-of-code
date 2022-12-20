6ca6e942b12ba0e7749acecaa851d10443d251e4
Rob Sassack <rsassack25@gmail.com>
2022-12-19 19:51:16 -0500

    Day 12 - Part 1 completed

---
+++ day12/day12.js
@@ -0,0 +1,96 @@
+const fs = require('fs');
+// read input file
+const file = fs.readFileSync('./input.txt', 'utf8');
+const input = file.split('\n');
+
+// split input into 2d array
+const graph = input.map((line) => {
+  return line.split('');
+});
+
+let start;
+let end;
+
+// set start and end positions
+for (let i = 0; i < graph.length; i++) {
+  for (let j = 0; j < graph[i].length; j++) {
+    if (graph[i][j] === 'S') {
+      start = [i, j];
+    } else if (graph[i][j] === 'E') {
+      end = [i, j];
+    }
+  }
+}
+
+// get height of current letter
+function height(letter) {
+  if (letter === 'S') {
+    return 0;
+  } else if (letter === 'E') {
+    return 25;
+  } else {
+    return letter.charCodeAt(0) - 97;
+  }
+}
+
+// get neighbors of current position
+function getNeighbors(x, y) {
+  let rows = graph.length;
+  let columns = graph[0].length;
+  let directions = [
+    [x - 1, y],
+    [x + 1, y],
+    [x, y - 1],
+    [x, y + 1],
+  ];
+  let valid = [];
+  for (let i = 0; i < directions.length; i++) {
+    const newX = directions[i][0];
+    const newY = directions[i][1];
+    // check if in bounds
+    if (newX >= 0 && newX < rows && newY >= 0 && newY < columns) {
+      // check if at most one higher
+      if (height(graph[newX][newY]) <= height(graph[x][y]) + 1) {
+        valid.push(directions[i]);
+      }
+    }
+  };
+  return valid;
+}
+
+// debug stuff
+// let testCoord = [1, 5];
+// let testNeighbors = neighbors(testCoord[0], testCoord[1]);
+// console.log(`${testCoord} - ${graph[testCoord[0]][testCoord[1]]} - ${height(graph[testCoord[0]][testCoord[1]])}`);
+// console.log('--------------');
+// testNeighbors.forEach((item) => {
+//   console.log(`${item} - ${graph[item[0]][item[1]]} - ${height(graph[item[0]][item[1]])}`);
+// })
+
+let visited = graph.map((row) => row.map((item) => false));
+let queue = [];
+
+// use bfs to find shortest path
+function bfs(start, end) {
+  queue.push([start, 0]);
+  while (queue.length > 0) {
+    let current = queue.shift();
+    let x = current[0][0];
+    let y = current[0][1];
+    let distance = current[1];
+    if (x === end[0] && y === end[1]) {
+      return distance;
+    }
+    if (!visited[x][y]) {
+      visited[x][y] = true;
+      let neighbors = getNeighbors(x, y);
+      neighbors.forEach((item) => {
+        queue.push([item, distance + 1]);
+      });
+    }
+  }
+  return -1;
+}
+
+// Part 1
+console.log("Shortest path: " + bfs(start, end));
