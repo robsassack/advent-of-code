7c53a215b5d9036573ff9b67727098e02b25ebc5
Rob Sassack <rsassack25@gmail.com>
2022-12-21 18:11:16 -0500

    Day 13 - Part 2 completed

--- day13/day13.js
+++ day13/day13.js
@@ -47,9 +47,14 @@ function compare(a, b) {
   }
 }
 
+let packets = [];
+
+// Part 1
 let sum = 0;
 for (let i = 0; i < pairs.length; i++) {
   // console.log(`=== Pair ${i+1} ===`);
+  packets.push(pairs[i][0]);
+  packets.push(pairs[i][1]);
   let answer = compare(pairs[i][0], pairs[i][1]);
   // console.log(answer);
   if (answer === true) {
@@ -58,3 +63,34 @@ for (let i = 0; i < pairs.length; i++) {
 }
 
 console.log("Sum of indicies of right order pairs: " + sum);
+
+// Part 2
+packets.push([[2]]);
+packets.push([[6]]);
+
+// sort the packets
+let sortedPackets = [];
+while (packets.length > 0) {
+  let min = packets[0];
+  let minIndex = 0;
+  for (let i = 1; i < packets.length; i++) {
+    if (compare(packets[i], min) === true) {
+      min = packets[i];
+      minIndex = i;
+    }
+  }
+  sortedPackets.push(min);
+  packets.splice(minIndex, 1);
+}
+
+let indexes = [];
+
+sortedPackets.forEach((packet, index) => {
+  if (JSON.stringify(packet) === "[[2]]") {
+    indexes.push(index+1);
+  } else if (JSON.stringify(packet) === "[[6]]") {
+    indexes.push(index+1);
+  }
+});
+
+console.log(indexes[0] * indexes[1]);
